13c396b277890cd874c6e7f004e14ebe981ba9be
Harish-Palaspagar <harishpalaspagar4@gmail.com>
2024-07-20 16:35:15 +0530

    initializing spring boot project

---
+++ src/main/java/com/harish/LearningBootApplication.java
@@ -0,0 +1,14 @@
+package com.harish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBootApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(LearningBootApplication.class, args);
+	}
+
+}
